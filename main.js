0ee70e0a2c59aa93692b12f8e41e19225588105d
Jorge Martinez <jorgeme0996@gmail.com>
2018-03-27 11:59:44 -0600

    entrando a segundo nodo

--- main.js
+++ main.js
@@ -4,10 +4,14 @@ let ref = firebase.database().ref("usuario");
 ref.once('value',function(data){
     let db=data.val()
     for(let i in db){
-        if (!db[i].email && db[i].perfilStripe && db[i].nombre){
-            // console.log(db[i].nombre, db[i].email, db[i].tarjeta, db[i].perfilStripe)
-            console.log(i)
-            noNameandEmail.push({i, nombre: db[i].nombre,email: db[i].email, tarjeta: db[i].tarjeta, Stripe: db[i].perfilStripe})
+        if (db[i].servicio){
+            for (let e in db[i].servicio){
+                if (db[i].servicio[e].tipo=="Agendado" && !db[i].servicio[e].periodicidad){
+                    // console.log(db[i].nombre, db[i].email, db[i].tarjeta, db[i].perfilStripe)
+                    console.log(i)
+                    noNameandEmail.push({i, nombre: db[i].nombre,email: db[i].email, tipo: db[i].servicio[e].tipo})
+                }
+            }
         }
     }
 console.log(noNameandEmail)
